77ae0faa73a304aa978287502533d8b8cb80553a
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-20 15:10:58 -0800

    Convert Products to a class component

--- src/pages/products.js
+++ src/pages/products.js
@@ -4,36 +4,41 @@ import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
 
-const Products = ({ data: { allContentfulProduct }}) => (
-  <Layout>
-    <div>
-      {/* Product List */}
-      <h2>Garb Products</h2>
-      {allContentfulProduct.edges.map(({ node: product }) => (
-        <div key={product.id}>
-          <Link 
-            to={`/products/${product.slug}`} 
-            style={{ 
-              textDecoration: "none",
-              color: "#551a8b"
-            }}
-          >
-            <h3 style={{ marginTop: '2rem' }}>
-              {product.name} • {` `}
-              <span style={{ fontSize: "1.2rem", fontWeight: 300, color: "#f60" }}>
-                ${product.price}
-              </span>
-            </h3>
-          </Link>
-          <Img 
-            style={{ maxWidth: 400}}
-            fluid={product.image.fluid}
-          />
+class Products extends React.Component {
+  render() {
+    const { data: { allContentfulProduct } } = this.props;
+    return (
+      <Layout>
+        <div>
+          {/* Product List */}
+          <h2>Garb Products</h2>
+          {allContentfulProduct.edges.map(({ node: product }) => (
+            <div key={product.id}>
+              <Link 
+                to={`/products/${product.slug}`} 
+                style={{ 
+                  textDecoration: "none",
+                  color: "#551a8b"
+                }}
+              >
+                <h3 style={{ marginTop: '2rem' }}>
+                  {product.name} • {` `}
+                  <span style={{ fontSize: "1.2rem", fontWeight: 300, color: "#f60" }}>
+                    ${product.price}
+                  </span>
+                </h3>
+              </Link>
+              <Img 
+                style={{ maxWidth: 400}}
+                fluid={product.image.fluid}
+              />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  </Layout>
-);
+      </Layout>
+    )
+  }
+};
 
 export const query = graphql`
 {
